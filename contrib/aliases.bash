2542a28232ba1d42e9dc70c2bce4fb693f9a8d9a
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-03-01 20:46:08 -0300

    Continua as configurações do projeto

---
+++ contrib/aliases.bash
@@ -0,0 +1,3 @@
+alias manage="python $PROJECT_ROOT/sgi/manage.py"
+alias cdsrc="cd $PROJECT_ROOT"
+alias cddjango="cd `virtualenvwrapper_get_site_packages_dir`/django"
--- src/sgi/settings.py
+++ src/sgi/settings.py
@@ -1,37 +1,37 @@
+import os
+from mongoengine import connect
+
+PROJECT_PATH = os.path.realpath(
+    os.path.join(os.path.dirname(__file__)))
+
+
 # Django settings for sgi project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Gustavo Hingel Morada', 'gusmorada@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default':{'ENGINE':'django.db.backends.sqlite3'}
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
+#MongoDB settings
+MONGO_DATABASE_NAME = 'sgi'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+connect(MONGO_DATABASE_NAME)
+
+TIME_ZONE = 'America/Sao_Paulo'
+LANGUAGE_CODE = 'pt-BR'
+
+
+USE_THOUSAND_SEPARATOR = True
+THOUSAND_SEPARATOR = '.'
+NUMBER_GROUPING = 3
 
 SITE_ID = 1
 
@@ -43,15 +43,6 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -62,16 +53,9 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'public', 'static'),    
 )
 
 # List of finder classes that know how to find static files in
@@ -79,7 +63,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -89,7 +72,12 @@ SECRET_KEY = 'kth52rhdeg5j*7*!wdmqvsl5625)7j!hk1@m1(75y^(*k1r&q*'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_STRING_IF_INVALID = 'TEMPLATE VARIABLE ERROR'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'templates')
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,12 +89,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'sgi.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
