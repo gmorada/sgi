2df75199845dc59ec02a5532605fe31f33633d2f
Gustavo Hingel Morada <gusmorada@gmail.com>
2012-03-02 00:02:38 -0300

    Cria model de imovel

--- src/sgi/home/models.py
+++ src/sgi/home/models.py
@@ -3,10 +3,19 @@
 from mongoengine import *
 
 __all__ = [
-    'Cliente'
+    'Cliente',
+    'Imovel'
 ]
 
 
 class Cliente(Document):
     nome = StringField(required=True)
     sobrenome = StringField(required=True)
+
+
+class Imovel(Document):
+    nome = StringField(required=True)
+    endereco = StringField(required=True)
+    aluguel = DecimalField(required=False)
+    proprietario = ReferenceField('Cliente', required=True)
+    inquilino = ReferenceField('Cliente', required=False)
